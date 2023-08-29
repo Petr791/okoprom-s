757a553316dd7fb3770a56e072de6b3820b75de3
petr791 <fisherman003210gmail.com>
2023-08-29 14:52:26 +0300

    next

--- js/index.js
+++ js/index.js
@@ -24,8 +24,6 @@ function removeMobileMenuClass() {
 }
 
 
-
-
 //
 if (document.documentElement.clientWidth > 576) {
     /* if (menu.classList.contains('active')) {
@@ -40,6 +38,7 @@ if (document.documentElement.clientWidth < 576) {
 
 $(document).ready(function() {
 
+    // форма поиска
     function siteSearch() {
 
         $('.search-form__btn').click(function() {
@@ -52,13 +51,54 @@ $(document).ready(function() {
     }
     siteSearch();
 
-    //
-
 });
-
 
 const formSearch = document.getElementById('search-form');
 
 formSearch.addEventListener('submit', function() {
     formSearch.releasePointerCapture();
 })
+
+
+
+
+//
+
+$(document).ready(function() {
+
+    // счетчики на странице "О компании"
+    $(window).scroll(function() {
+        scrollTracking();
+    });
+
+    let block_show = false;
+
+    function scrollTracking() {
+        if (block_show) {
+            return false;
+        }
+        let windowScrollTop = $(window).scrollTop();
+        let windowHeight = $(window).height();
+        let elementOffset = $('.section-benefit').offset().top;
+        let elementHeight = $('.section-benefit').outerHeight();
+        let documentHeight = $(document).height();
+
+        if (windowScrollTop + windowHeight >= elementOffset || windowHeight + windowScrollTop == documentHeight || elementHeight + elementOffset < windowHeight) {
+            block_show = true;
+
+            // анимация счетчиков на странице "О компании"
+            $('.benefit-counter').each(function() {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 1500,
+                    easing: 'swing',
+                    step: function(now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+            });
+        }
+    }
+    scrollTracking();
+});
