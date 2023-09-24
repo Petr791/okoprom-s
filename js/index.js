b27ddf0d20095cd538677ab923599a2edab07fc3
petr791 <fisherman003210gmail.com>
2023-09-24 22:52:44 +0300

    modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -36,6 +36,8 @@ if (document.documentElement.clientWidth < 576) {
 
 }
 
+
+
 $(document).ready(function() {
 
     // форма поиска
@@ -62,16 +64,143 @@ formSearch.addEventListener('submit', function() {
 
 
 
-//
+// фильтры 
 
-const mobileFiltersButton = document.getElementById('filters-btn');
+const mobileFiltersButton = document.getElementById('filters');
 const mobileFiltersBlock = document.getElementById('filters-block');
+const filtersCasing = document.querySelector('.filters-casing');
+let parentfilters = document.querySelector('.filters-wrapper');
+let filtersBtns = document.querySelectorAll('.filters-btn');
 
-console.log(mobileFiltersButton);
-console.log(mobileFiltersBlock);
-mobileFiltersButton.addEventListener('click', function() {
-    console.log('hhhhh');
-    mobileFiltersBlock.classList.toggle('active');
+/* 
+for (i = 0; i < parentfilters.length; index++) {
+    const elem = parentfilters[index];
 
+    elem.addEventListener('click', (event) => {
+        // Отлавливаем элемент в родители на который мы нажали
+        let targetEl = event.target;
+
+        // Проверяем тот ли это элемент который нам нужен
+        if (targetEl.classList.contains('filters-btn')) {
+            for (let i = 0; i < filtersBtns.length; i++) {
+                // Убираем у других
+                filtersBtns[i].classList.remove('active');
+            }
+            // Добавляем тому на который нажали
+            targetEl.classList.add('active');
+            //console.log(targetEl);
+            body.classList.remove('lock');
+            let filterLink = targetEl.getAttribute("data-page");
+            document.location.href = filterLink;
+            //console.log(filterLink);
+        }
+
+    });
+
+}
+ */
+
+
+parentfilters.addEventListener('click', (event) => {
+    // Отлавливаем элемент в родители на который мы нажали
+    let targetEl = event.target;
+
+    // Проверяем тот ли это элемент который нам нужен
+    if (targetEl.classList.contains('filters-btn')) {
+        for (let i = 0; i < filtersBtns.length; i++) {
+            // Убираем у других
+            filtersBtns[i].classList.remove('active');
+        }
+        // Добавляем тому на который нажали
+        targetEl.classList.add('active');
+        body.classList.remove('lock');
+        let filterLink = targetEl.getAttribute("data-page");
+        document.location.href = filterLink;
+    }
 
 });
+
+mobileFiltersButton.addEventListener('click', function() {
+    mobileFiltersBlock.classList.toggle('active');
+    filtersCasing.classList.toggle('active');
+    body.classList.add('lock');
+
+});
+
+
+
+// кнопка "Загрузить еще"
+const btnMoreProducts = document.getElementById('btn-more');
+let productsCards = Array.from(document.querySelectorAll('.catalog-card'));
+
+window.addEventListener('resize', function(event) {
+    if (event.target.window.innerWidth > 1024) response1();
+    if (event.target.window.innerWidth <= 1024 && event.target.window.innerWidth > 576) response2();
+    if (event.target.window.innerWidth < 576) response3();
+});
+
+
+function openCatalog() {
+    btnMoreProducts.addEventListener('click', () => {
+        productsCards.forEach(item => item.classList.remove('hidden'));
+        btnMoreProducts.classList.add('hidden');
+    });
+};
+
+function response1() {
+    if (window.innerWidth > 1024) {
+
+        btnMoreProducts.classList.add('hidden');
+
+        productsCards.forEach((item, index) => {
+            item.classList.add('hidden');
+            if (index <= 8) {
+                item.classList.remove('hidden');
+            } else if (index > 8) {
+                btnMoreProducts.classList.remove('hidden');
+            }
+            openCatalog();
+        })
+    }
+}
+
+response1();
+
+
+function response2() {
+    if (window.innerWidth <= 1024 && window.innerWidth > 576) {
+
+        btnMoreProducts.classList.add('hidden');
+
+        productsCards.forEach((item, index) => {
+            item.classList.add('hidden');
+            if (index <= 7) {
+                item.classList.remove('hidden');
+            } else if (index > 7) {
+                btnMoreProducts.classList.remove('hidden');
+            }
+            openCatalog();
+        })
+    }
+}
+
+response2();
+
+function response3() {
+    if (window.innerWidth < 576) {
+
+        btnMoreProducts.classList.add('hidden');
+
+        productsCards.forEach((item, index) => {
+            item.classList.add('hidden');
+            if (index <= 5) {
+                item.classList.remove('hidden');
+            } else if (index > 5) {
+                btnMoreProducts.classList.remove('hidden');
+            }
+            openCatalog();
+        })
+    }
+}
+
+response3();
